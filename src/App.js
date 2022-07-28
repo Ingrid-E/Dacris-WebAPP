45011aa11d1b8fd85096fbf3edfcaf8b246930f8
jhan-ai-s13 <jhan.perez@correounivalle.edu.co>
2022-07-28 01:40:28 -0500

    login v1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {Home, AboutUs, Login, Products} from "./pages/index"
 import { Navbar, SocialMedia } from './components';
-import './app.css'
-
+import './app.css';
+import Login from "./pages/login/login";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <div className="App">
-      <Products></Products>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </Router>
       </div>
 
   );
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import {Helmet} from "react-helmet";
+import { Button } from 'react-bootstrap';
+import "./login.css";
+
+import logo from "./img/Marca de Agua.png";
+import portada from "./img/FOTO FERIA 2.png";
+//import "./css/bootstrap.min.css";
 
 function Login(){
     return(
-        <div className="login">
-            <h1>Login</h1>
-        </div>
+        <>
+
+        <helmet>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <script src="js/jquery-1.11.3.min.js"/>
+            <script src="js/bootstrap.min.js"/>
+            <script src="js/script.js"/>
+        </helmet>
+
+         <div id="mainImg">
+            <img src={portada} id="imgPr"/>
+         </div>
+  
+  <section id="agua">
+    <img src={logo} id="marAgua"/>
+  </section>
+  
+  <section id="login">
+    <h2>Iniciar Sesión</h2>
+    <form className="verificar">
+      <div className="form-group">
+        <label for="exampleInputEmail1" id="user">Usuario</label>
+        <input type="email" className="form-control" id="user" aria-describedby="emailHelp"/>
+      </div>
+      <div className="form-group">
+        <label for="exampleInputPassword1" id="password">Contraseña</label>
+        <input type="password" className="form-control" id="password"/>
+      </div>
+      <button id="iniciar" type="submit" className="btn btn-primary">Iniciar sesion</button>
+    </form>
+  </section>
+</>
     )
 }
 
